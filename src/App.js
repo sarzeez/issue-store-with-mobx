0682cba9d37e907b7b266b3dd38c65092f9abeef
Abdulaziz Salokhiddinov <abdulaziz_salokhiddinov@epam.com>
2022-02-01 23:26:03 +0500

    issue store mobx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import IssueStore from "./mobx/IssueStore";
+import IssueForm from "./mobx/IssueForm";
+import UserList from "./mobx/UserList";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IssueStore>
+        <IssueForm />
+        <UserList />
+      </IssueStore>
     </div>
   );
 }
